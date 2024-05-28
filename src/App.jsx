09bd4788d519c926b35b955bb2a6020b5ae4dff0
Amegh <80452444+amegh-ts@users.noreply.github.com>
2024-05-28 15:23:15 +0530

    28-05-2024

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import './App.css'
+import Breadcrumb from './components/Breadcrumb'
+import Details from './components/Details'
 
 function App() {
 
   return (
     <div className='container'>
-      
+      <Breadcrumb />
+      <Details />
     </div>
   )
 }
