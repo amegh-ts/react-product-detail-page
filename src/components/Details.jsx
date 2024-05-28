d963d7ad1c56ac8d36cbe7093f82a6f6bd4b8bb4
Amegh <80452444+amegh-ts@users.noreply.github.com>
2024-05-28 15:42:22 +0530

    Update Details.jsx

--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -15,10 +15,32 @@ const Details = () => {
 
                     <a href="#!" className='prev'>&#10094;</a> 
                     <a href="#!" className='next'>&#10095;</a>
+
+                    <div className="slider-img">
+                        {
+                            product.images.map((image,index)=>(
+                                <div className="slider-box">
+                                    <img src={image.src} alt="" />
+                                </div>
+                            ))
+                        }
+                    </div>
                 </div>
 
                 <div className='product-page-details'>
-
+                    <strong>{product.name}</strong>
+                    <p className='product-category'>{product.brand} - {product.category}</p>
+                    <p className="product-price">
+                        ${Math.round(product.price - (product.price * product.discount) / 100)} <del>${product.price}</del>
+                    </p>
+                    <p className="small-desc">{product.desc}</p>
+                    <div className="product-options">
+                        {product.colors.map(color => (
+                            <div key={color}>
+                                <button></button>
+                            </div>
+                        ))}
+                    </div>
                 </div>
             </section>
 
