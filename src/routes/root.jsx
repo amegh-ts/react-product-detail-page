6d41c78e5bbd8bde435150c4e8e0ff11bf51dea4
Amegh <80452444+amegh-ts@users.noreply.github.com>
2024-06-03 10:36:05 +0530

    03-06-2024

---
+++ src/routes/root.jsx
@@ -0,0 +1,45 @@
+import { Outlet } from "react-router-dom";
+// import Header from "../components/Header";
+// import Footer from "../pages/Footer/Footer";
+import { useEffect, useState } from "react";
+// import { IoArrowUpCircleSharp } from 'react-icons/io5';
+
+export default function Root() {
+//   const [showScrollButton, setShowScrollButton] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollPosition = window.scrollY;
+      const scrollThreshold = 400;
+      setShowScrollButton(scrollPosition > scrollThreshold);
+    };
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
+  };
+
+  return (
+    <>
+    <div>
+      {/* <Header /> */}
+      <div className="content">
+        <Outlet />
+      </div>
+      {/* <Footer /> */}
+      {/* {showScrollButton && (
+        <div className="scroll-to-top" onClick={scrollToTop}>
+          <IoArrowUpCircleSharp className='scroll-icon' />
+        </div>
+      )} */}
+      </div>
+    </>
+  );
+}
